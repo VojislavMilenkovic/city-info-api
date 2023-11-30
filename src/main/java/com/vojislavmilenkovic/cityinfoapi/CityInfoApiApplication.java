0aca8e418087e1882d1a45c0b7ad6b9b59016492
Vojislav <vojislav.milenkovic@eng.it>
2023-11-30 14:27:09 +0100

    Init commit

---
+++ src/main/java/com/vojislavmilenkovic/cityinfoapi/CityInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vojislavmilenkovic.cityinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vojislavmilenkovic/cityinfoapi/CityInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vojislavmilenkovic.cityinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
